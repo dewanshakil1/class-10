471c7359ec2708b81bf8fcf3a8283ac01b61ea7c
skakil mia <dewanshakil88@gmail.com>
2022-05-15 20:19:58 +0600

    uploade all file

---
+++ all assignment.js
@@ -0,0 +1,29 @@
+//email pattern=======
+
+// let txt="dewanshail88@gmail.com";
+// let pattern=/^[a-zA-Z0-9]*@[a-z]*\.[a-z]{2,5}$/;
+// console.log(pattern.test(txt));
+
+//username pattern======
+
+let Username="shakil-";
+let pattern= /^[a-z\-]*$/;
+console.log(pattern.test(Username));
+
+//Bangladeshi phone number pattern===
+
+// let number="+8801851246924";
+// let pattern= /^(01|\+8801|8801)[0-9]{9}$/;
+// console.log(pattern.test(number));
+
+// password pattern =====
+
+// let password="shakil5124";
+// let pattern= /^[a-zA-Z0-9!@#$%]{6,16}$/;
+// console.log(pattern.test(password));
+
+//zipcode pattern  =====
+
+// let zipcode="1234";
+// let pattern= /^\d{4}$/;
+// console.log(pattern.test(zipcode));
